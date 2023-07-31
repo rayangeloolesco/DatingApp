6abcb5f1ca1e4d95aec091726910cec421cd92ac
Ray Olesco <raolesco@yahoo.com>
2023-07-31 21:36:37 +0800

    End of section 14

--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -1,12 +1,25 @@
 import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { Member } from 'src/app/_models/member';
+import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
   selector: 'app-member-card',
   templateUrl: './member-card.component.html',
   styleUrls: ['./member-card.component.css'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
 export class MemberCardComponent {
   @Input() member: Member | undefined;
+
+  constructor(
+    private memberService: MembersService,
+    private toastr: ToastrService
+  ) {}
+
+  addLike(member: Member) {
+    this.memberService.addLikes(member.userName).subscribe({
+      next: () => this.toastr.success('You have like ' + member.knownAs),
+    });
+  }
 }
