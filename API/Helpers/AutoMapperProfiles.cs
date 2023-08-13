98e41f5f3b6b8e998056840b1cc1495fb5ca7260
Ray Olesco <raolesco@yahoo.com>
2023-08-13 09:42:05 +0800

    Section 15: Lesson 160

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -5,18 +5,31 @@ using AutoMapper;
 
 namespace API.Helpers
 {
-    public class AutoMapperProfiles: Profile
+    public class AutoMapperProfiles : Profile
     {
         public AutoMapperProfiles()
         {
             CreateMap<AppUser, MemberDto>()
-            .ForMember(dest => dest.PhotoUrl, opt => opt
-            .MapFrom(src => src.Photos.FirstOrDefault(x=>x.IsMain).Url))
-            .ForMember(dest => dest.Age, opt => opt
-                .MapFrom(scr => scr.DateOfBirth.CalculateAge()));
+                .ForMember(
+                    dest => dest.PhotoUrl,
+                    opt => opt.MapFrom(src => src.Photos.FirstOrDefault(x => x.IsMain).Url)
+                )
+                .ForMember(
+                    dest => dest.Age,
+                    opt => opt.MapFrom(scr => scr.DateOfBirth.CalculateAge())
+                );
             CreateMap<Photo, PhotoDto>();
             CreateMap<MemberUpdateDto, AppUser>();
             CreateMap<RegisterDto, AppUser>();
+            CreateMap<Message, MessageDto>()
+                .ForMember(
+                    d => d.SenderPhotoUrl,
+                    o => o.MapFrom(s => s.Sender.Photos.FirstOrDefault(x => x.IsMain).Url)
+                )
+                .ForMember(
+                    d => d.RecipientPhotoUrl,
+                    o => o.MapFrom(s => s.Recipient.Photos.FirstOrDefault(x => x.IsMain).Url)
+                );
         }
     }
 }
